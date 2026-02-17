9e05752d28dc5f56737736e01a126b1a3669c59e
Soney <sonephyo7777777@gmail.com>
2026-02-17 13:01:10 -0500

    feat: populate map with geojson points

--- suny-oswego-map-frontend/app/map/map.tsx
+++ suny-oswego-map-frontend/app/map/map.tsx
@@ -1,12 +1,12 @@
-import Map from "react-map-gl/maplibre";
+import Map, { Layer, Source } from "react-map-gl/maplibre";
 import "maplibre-gl/dist/maplibre-gl.css";
 import React from "react";
 
 export default function MapSunyOswego() {
   const [viewState, setViewState] = React.useState({
-    longitude:  -76.547837,
-    latitude: 43.450660,
-    zoom: 15,
+    longitude: -76.54896,
+    latitude: 43.45191,
+    zoom: 14.5,
   });
 
   return (
@@ -15,6 +15,23 @@ export default function MapSunyOswego() {
       onMove={(evt) => setViewState(evt.viewState)}
       style={{ width: "100vw", height: "100vh" }}
       mapStyle={`https://api.maptiler.com/maps/streets-v4/style.json?key=${import.meta.env.VITE_MAPLIBRE_KEY}`}
-    />
+    >
+      <Source
+        id="suny-oswego-data"
+        type="geojson"
+        data={"/suny-oswego.geojson"}
+      >
+        <Layer
+          id="campus-points"
+          type="circle"
+          paint={{
+            "circle-radius": 8,
+            "circle-color": "#245c38",
+            "circle-stroke-color": "#ffffff",
+            "circle-stroke-width": 2,
+          }}
+        />
+      </Source>
+    </Map>
   );
 }
